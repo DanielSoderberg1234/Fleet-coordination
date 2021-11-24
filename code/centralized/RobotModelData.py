c15226968c6512fe75990941aa7273515fa74465
margor22 <martin.gordon@telia.com>
2021-11-24 09:55:58 +0100

    M: added comments

--- code/centralized/RobotModelData.py
+++ code/centralized/RobotModelData.py
@@ -16,13 +16,16 @@ class RobotModelData:
     qN: float = 100         # Final cost of deviating from x and y reference
     qthetaN: float = 10     # Final cost of deviating from angle reference
     qobs: float = 200       # Cost for being closer than r to the other robot
-    qaccV: float = 100
-    qaccW: float = 100
-    qpol: float = 200
-    qbounds: float = 200
+    qaccV: float = 100      # Cost for acelerating and decelerating
+    qaccW: float = 100      # Cost for angular acelaration
+    qpol: float = 200       # Cost for being inside obstacle (polygon)
+    qbounds: float = 200    # Cost four being out of bounds
 
 
     def get_weights(self):
+        """
+        Get all weights as a list.
+        """
         return [self.q, self.qtheta, self.r, self.qN, self.qthetaN, self.qobs, self.qaccV, self.qaccW, self.qpol, self.qbounds]
 
 
